1085c658f022e20eb7a4a5f4a170e83ef933a885
Francisco Mesar <fmesar87@gmail.com>
2022-05-01 14:28:31 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e)=> {
+    e.preventDefault()
+    handletodo(e.target.new_task_description.value)
+  })
+
+
 });
+
+
+
+function handletodo(todo){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handledelete)
+  btn.textContent = 'x'
+  li.textContent = `${todo}   `
+  li.style.color = 'white'
+  li.style.fontSize = '25px'
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+}
+function handledelete(e){
+ e.target.parentNode.remove()
+}
